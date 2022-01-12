ad450c579cbfcdb7b11815b6d6ce783561ea7a79
mayankuppal21104035 <97498786+mayankuppal21104035@users.noreply.github.com>
2022-01-12 17:18:54 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,78 @@
+
+
+#answer 1
+#enter the three numbers
+print('ANSWER 1')
+i=input('enter number 1: ');
+j=input('enter number 2: ');
+k=input('enter number 3: ');
+#finding the average of these numbers
+avg=(int(i)+int(j)+int(k))/3
+#final answer
+print('the average of these numbers is: '+str(avg))
+
+print()
+
+
+#answer 2
+print('ANSWER 2')
+#getting input from the user 
+i=input('enter your gross income: ')
+l=input('enter number of decendents: ')
+
+#gross income 
+ti=int(i)-10000-(3000*int(l))
+#finding the tax
+tax=ti*1/5
+#getting the final tax
+print('Tax is: ',tax)
+
+
+print()
+
+#answer 3
+print('ANSWER 3')
+#getting input from student
+sid=int(input('enter your SID: '))
+a=input('enter your name: ')
+b=input('enetr your gender(F/M/U): ')
+cn=input('enter your coursename: ')
+cg=float(input('enter your CGPA: '))
+#forming the list
+student=[sid,a,b,cn,cg]
+#final answer
+print('Student:',student)
+
+print()
+
+#answer 4
+print('ANSWER 4')
+#getting the marks of students
+s1=int(input('enter the marks of 1st student:'))
+s2=int(input('enter the marks of 2nd student:'))
+s3=int(input('enter the marks of 3rd student:'))
+s4=int(input('enter the marks of 4th student:'))
+s5=int(input('enter the marks of 5th student:'))
+#getting it in the form of  list
+list=[s1,s2,s3,s4,s5]
+#final answer
+print('Marks of 5 students are:',list)
+
+print()
+
+#answer 5(a)
+print('ANSWER 5(a)')
+#writing all the colors
+color=['Red','Green','White','Black','Pink','Yellow']
+#removing the 4th element
+color.remove('Black')
+#getting the final output
+print(color)
+
+
+#answer 5(b)
+print('ANSWER 5(b)')
+#removing 'Black' and 'Pink' and getting purple
+color[3:5]={'Purple'}
+#final output
+print(color)
---
+++ output assignment 1.py
@@ -0,0 +1,37 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:\Users\HP\Desktop\assignment 1.py =================
+ANSWER 1
+enter number 1: 5
+enter number 2: 5
+enter number 3: 5
+the average of these numbers is: 5.0
+
+ANSWER 2
+enter your gross income: 20000
+enter number of decendents: 2
+Tax is:  800.0
+
+ANSWER 3
+enter your SID: 21104035
+enter your name: Mayank
+enetr your gender(F/M/U): M
+enter your coursename: Python
+enter your CGPA: 9.5
+Student: [21104035, 'Mayank', 'M', 'Python', 9.5]
+
+ANSWER 4
+enter the marks of 1st student:15
+enter the marks of 2nd student:20
+enter the marks of 3rd student:16
+enter the marks of 4th student:14
+enter the marks of 5th student:19
+Marks of 5 students are: [15, 20, 16, 14, 19]
+
+ANSWER 5(a)
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+ANSWER 5(b)
+['Red', 'Green', 'White', 'Purple']
+>>> 
+================= RESTART: C:\Users\HP\Desktop\assignment 1.py =================
